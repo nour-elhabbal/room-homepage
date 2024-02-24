d03efb3017ed8b164694c20e2586d0d51742a0b8
nourahmed <105996059+nour-elhabbal@users.noreply.github.com>
2024-02-24 23:55:25 +0000

    Feat: added global styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
 
+import "./styles/globalStyles.scss";
+
 const root = createRoot(document.getElementById("root"));
 
 root.render(<App />);
