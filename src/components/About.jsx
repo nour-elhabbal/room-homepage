a6ef85b007edf868ab8a90d4ab3c507706c4855d
nourahmed <105996059+nour-elhabbal@users.noreply.github.com>
2024-02-26 01:36:31 +0000

    Feat: Added the about section

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import "../styles/About.scss";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="img1"></div>
+      <div className="content">
+        <h3>about our furniture</h3>
+        <p>
+          Our multifunctional collection blends design and function to suit your
+          individual taste. Make each room unique, or pick a cohesive theme that
+          best express your interests and what inspires you. Find the furniture
+          pieces you need, from traditional to contemporary styles or anything
+          in between. Product specialists are available to help you create your
+          dream space.
+        </p>
+      </div>
+      <div className="img2"></div>
+    </div>
+  );
+};
+
+export default About;
